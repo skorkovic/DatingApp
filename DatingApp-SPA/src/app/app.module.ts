a7bdb5eaf8cbca465116753a4ce93f5d6fea0c1f
Srdjan Skorkovic <skorkovic1996@gmail.com>
2020-01-02 16:02:44 +0100

    End of section 11

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -28,6 +28,8 @@ import { NgxGalleryModule } from 'ngx-gallery';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver ';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -45,6 +47,7 @@ export function tokenGetter() {
       MemberCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
+      PhotoEditorComponent,
    ],
    imports: [
       BrowserModule,
@@ -62,6 +65,7 @@ export function tokenGetter() {
       }),
       TabsModule.forRoot(),
       NgxGalleryModule,
+      FileUploadModule,
    ],
    providers: [
       AuthService,
--- DatingApp.API/Dtos/PhotoForReturnDto.cs
+++ DatingApp.API/Dtos/PhotoForReturnDto.cs
@@ -1,8 +1,8 @@
 using System;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class Photo
+    public class PhotoForReturnDto
     {
         public int Id { get; set; }
 
@@ -14,10 +14,6 @@ namespace DatingApp.API.Models
 
         public bool IsMain { get; set; }
 
-        public string PublicID { get; set; }
-
-        public User User { get; set; }
-
-        public int UserId { get; set; }
+        public string PublicId { get; set; }
     }
 }
