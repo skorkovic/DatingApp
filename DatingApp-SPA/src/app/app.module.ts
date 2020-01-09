69ff13cefcf02bf44fa140d7f50558ff96454f08
Srdjan Skorkovic <skorkovic1996@gmail.com>
2020-01-09 07:54:55 +0100

    End of section 13

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver ';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -48,6 +49,7 @@ export function tokenGetter() {
       MemberDetailComponent,
       MemberEditComponent,
       PhotoEditorComponent,
+      TimeAgoPipe,
    ],
    imports: [
       BrowserModule,
